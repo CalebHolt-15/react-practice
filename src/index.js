732e6094af841c2ff269f7b06c02b2ac0ce033d0
calebholt-15 <calebholt100@gmail.com>
2020-09-28 10:04:43 +0530

    Taking turns

--- src/index.js
+++ src/index.js
@@ -2,18 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
+// class Square extends React.Component {
+//
+// //state
+//     render() {
+//         return (
+//             <button
+//                 className="square"
+//                 onClick={()=>this.props.onClick()} >
+//                 {this.props.value}
+//             </button>
+//         );
+//     }
+// }
 
-//state
-    render() {
-        return (
-            <button
-                className="square"
-                onClick={()=>this.props.onClick()} >
-                {this.props.value}
-            </button>
-        );
-    }
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}>
+            {props.value}
+        </button>
+    );
 }
 
 class Board extends React.Component {
@@ -21,14 +29,17 @@ class Board extends React.Component {
         super(props);
             this.state={//json
                 squares:Array(9).fill(null)
+                xIsNext: true,
             };
     }
 
 
     handleClick(i){
         const squares = this.state.squares.slice();
-        squares[i] = 'X';
-        this.setState({ squares: squares });
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ squares: squares,
+            xIsNext: !this.state.xIsNext,
+        });
     }
 
 
