e5b2b8682a83a1abd8f87518bcbce5fcab3dab79
calebholt-15 <calebholt100@gmail.com>
2020-09-28 19:07:40 +0530

    kitty

--- src/index.js
+++ src/index.js
@@ -1,3 +1,97 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+
+const user = { //json.
+
+
+    firstName: 'Harper',
+    lastName: 'Perez'
+};
+
+function formatName(user) {
+    return user.firstName + ' ' + user.lastName;
+}
+
+const element = (
+    <h1>
+        Hello, {formatName(user)}!
+    </h1>
+);
+
+function getGreeting(user) {
+    if (user) {
+        return <h1>Hello, {formatName(user)}!</h1>;
+    }
+    return <h1>Hello, Stranger.</h1>;
+}
+
+//....................
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    // ReactDOM.render(element, document.getElementById('root'));
+}
+
+setInterval(tick, 1000);
+
+//.......................
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo user={props.author}/>
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+             src={props.user.avatarUrl}
+             alt={props.user.name}
+        />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar user={props.user}/>
+            <div className="UserInfo-name">
+                {props.user.name}
+            </div>
+        </div>
+    );
+}
+
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'https://placekitten.com/g/64/64',
+    },
+};
+
+ReactDOM.render(
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author}
+    />,
+    document.getElementById('root')
+);
