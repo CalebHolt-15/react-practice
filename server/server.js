8b62682ef995d36c314e7b4bb237e44e9e7f0161
calebholt-15 <calebholt100@gmail.com>
2020-09-30 14:51:05 +0530

    mongodb connected

--- server/server.js
+++ server/server.js
@@ -1,14 +1,12 @@
-const http= require('http')
+const express = require('express');
+const mongoose = require('mongoose');
 
-http.createServer(function (request, response) {
-    // Send the HTTP header
-    // HTTP Status: 200 : OK
-    // Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true, useUnifiedTopology: true})
 
-    // Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8081);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('mongodb connected')
+});
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+
