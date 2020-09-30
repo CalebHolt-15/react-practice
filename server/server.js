54c895daf7791c7c6096341661f7ed242982045a
calebholt-15 <calebholt100@gmail.com>
2020-09-30 15:06:43 +0530

    add data to database

--- server/server.js
+++ server/server.js
@@ -9,4 +9,15 @@ db.once('open', function() {
     console.log('mongodb connected')
 });
 
+// schema.................................
+const kittySchema = new mongoose.Schema({
+    name: String
+});
 
+//model......................modelname;kitten
+const Kitten = mongoose.model('Kitten', kittySchema);
+
+const silence = new Kitten({ name: 'Silence' });
+console.log(silence.name); // 'Silence'
+
+silence.save()
