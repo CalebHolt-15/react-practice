3be3d4e180eab67b9679fb4d88954af636ed6013
calebholt-15 <calebholt100@gmail.com>
2020-09-30 16:03:37 +0530

    added to db

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+var app = express();
+
 mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true, useUnifiedTopology: true})
 
 const db = mongoose.connection;
@@ -17,7 +19,32 @@ const kittySchema = new mongoose.Schema({
 //model......................modelname;kitten
 const Kitten = mongoose.model('Kitten', kittySchema);
 
-const silence = new Kitten({ name: 'Silence' });
-console.log(silence.name); // 'Silence'
 
-silence.save()
+// app.use(express.static('./public'));
+
+///   express............
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+    res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.get('/add_kitten', function (req, res) {
+    // Prepare output in JSON format
+    console.log(req)
+    const newkitten = new Kitten({name:req.query.kitten_name})
+
+    newkitten.save(function(err,kitten){
+        if(err)return console.error(err);
+        console.log(kitten.name);
+        res.send(kitten.name)
+    })
+})
+
+var server = app.listen(8081, function () {
+    var host = server.address().address
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port)
+})
