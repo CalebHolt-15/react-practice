b4d2cfc2bb77468beb33584a2c60448b0d3d2448
calebholt-15 <calebholt100@gmail.com>
2020-10-01 13:36:34 +0530

    Basic LoginForm with username and password

--- server/server.js
+++ server/server.js
@@ -1,73 +1,26 @@
-const express = require('express');
-const mongoose = require('mongoose');
-
+var express = require('express');
 var app = express();
-
 var bodyParser = require('body-parser');
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true, useUnifiedTopology: true})
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-    console.log('mongodb connected')
-});
-
-// schema.................................
-const UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
-});
-
-//model......................modelname;kitten
-const User = mongoose.model('User', UserSchema);
-
-
-// app.use(express.static('./public'));
-
-///   express............
-
 app.use(express.static('public'));
 
-app.get('/index.html', function (req, res) {
-    res.sendFile( __dirname + "/" + "index.html" );
+app.get('/src/index.js', function (req, res) {
+    res.sendFile( __dirname + "/" + "/src/index.js" );
 })
 
-app.post('/adduser',urlencodedParser, function (req, res) {
+
+app.post('/process_post', urlencodedParser, function (req, res) {
     // Prepare output in JSON format
-    console.log(req)
-    const newuser = new User({username:req.body.username, password:req.body.password})
-
-    newuser.save(function(err,user){
-        if(err)return console.error(err);
-        console.log(user.username);
-        res.send('registered user')
-    })
+    response = {
+        first_name:req.body.first_name,
+        last_name:req.body.last_name
+    };
+    console.log(response);
+    res.end(JSON.stringify(response));
 })
-
-app.get('/userlogin',urlencodedParser, function(req,res){
-
-
-    User.findOne({ username: req.body.username }, function (err, user) {
-        if(err){
-            res.send('error is found')
-        }
-        if(user){
-            if(user.password===req.body.password){
-                res.send('welcom back')
-            }else{
-                res.send('incorect pasword')
-            }
-        }
-        else{
-            res.send('user not exist')
-        }
-    });
-} )
-
 
 var server = app.listen(8081, function () {
     var host = server.address().address
@@ -76,3 +29,79 @@ var server = app.listen(8081, function () {
     console.log("Example app listening at http://%s:%s", host, port)
 })
 
+
+
+
+
+
+// var bodyParser = require('body-parser');
+//
+// // Create application/x-www-form-urlencoded parser
+// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+//
+// mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser:true, useUnifiedTopology: true})
+//
+// const db = mongoose.connection;
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//     console.log('mongodb connected')
+// });
+//
+// // schema.................................
+// const UserSchema = new mongoose.Schema({
+//     username: String,
+//     password: String
+// });
+//
+// //model......................modelname;kitten
+// const User = mongoose.model('User', UserSchema);
+//
+//
+// // app.use(express.static('./public'));
+//
+// ///   express............
+//
+// app.use(express.static('public'));
+//
+//
+//
+// app.post('/adduser',urlencodedParser, function (req, res) {
+//     // Prepare output in JSON format
+//     console.log(req)
+//     const newuser = new User({username:req.body.username, password:req.body.password})
+//
+//     newuser.save(function(err,user){
+//         if(err)return console.error(err);
+//         console.log(user.username);
+//         res.send('registered user')
+//     })
+// })
+//
+// app.get('/userlogin',urlencodedParser, function(req,res){
+//
+//
+//     User.findOne({ username: req.body.username }, function (err, user) {
+//         if(err){
+//             res.send('error is found')
+//         }
+//         if(user){
+//             if(user.password===req.body.password){
+//                 res.send('welcom back')
+//             }else{
+//                 res.send('incorect pasword')
+//             }
+//         }
+//         else{
+//             res.send('user not exist')
+//         }
+//     });
+// } )
+//
+//
+// var server = app.listen(8081, function () {
+//     var host = server.address().address
+//     var port = server.address().port
+//
+//     console.log("Example app listening at http://%s:%s", host, port)
+// })
+//
--- src/index.js
+++ src/index.js
@@ -2,9 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-// ========================================
+class LoginForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {username: '', password:''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    UserName:
+                    <input type="text" name = "username" value={this.state.username} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <label>
+                    Password:
+                    <input type="text" name = "password" value={this.state.password} onChange={this.handleChange} />
+                </label>
+                <br/>
+                <input type="submit" value="Submit" />
+            </form>
+        );
+    }
+}
 
 ReactDOM.render(
-    <h1>hello </h1>,
+    <LoginForm />,
     document.getElementById('root')
 );
