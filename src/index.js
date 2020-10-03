0c22d891495bac81ba958ace2c45b4ba52db51d6
calebholt-15 <calebholt100@gmail.com>
2020-10-03 17:38:10 +0530

    hooks

--- src/index.js
+++ src/index.js
@@ -1,82 +1,23 @@
+// eslint-disable-next-line
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { useState, useEffect } from 'react';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state={
-            value:null
-        }
-    }
-//state
-    render() {
-        return (
-            <button className="square"
-                    onClick={()=>this.setState({value:'X'})}>
-                {this.state.value}
-            </button>
-        );
-    }
-}
 
-class Board extends React.Component {
-    constructor(props){
-        super(props);
-            this.state={//json
-                squares:Array(9).fill(null)
-            };
-    }
+let key = 'z';
+let {[key]: foo} = {z: 'bar'};
 
-    renderSquare(i) {
-        return <Square value={i}/>;
-    }
+// console.log(foo); // "bar"
+// console.log([key]); //
 
-    render() {
-        const status = 'Next player: X';
+let {a, b, ...rest} = {a: 10, b: 20, c: 30, d: 40}
+// console.log(a); //
+// console.log(b); //
+//
+// console.log(rest); // { c: 30, d: 40 }
 
-        return (
-            <div>
-                <div className="status">{status}</div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
-    }
-}
+let obj = {self: '123'};
+obj.__proto__.prot = '456';
+const {self, prot} = obj;
+// console.log(self); //
+// console.log(b); //
 
-class Game extends React.Component {
-    render() {
-        return (
-            <div className="game">
-                <div className="game-board">
-                    <Board />
-                </div>
-                <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
-                </div>
-            </div>
-        );
-    }
-}
-
-// ========================================
-
-ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-);
