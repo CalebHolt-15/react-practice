6ea0b0a7ff5a85dae9d47e17cc729dcfc346f60d
calebholt-15 <calebholt100@gmail.com>
2020-10-03 18:21:28 +0530

    usestate hook

--- src/index.js
+++ src/index.js
@@ -21,3 +21,19 @@ const {self, prot} = obj;
 // console.log(self); //
 // console.log(b); //
 
+
+// ....hooks.....
+
+function Example() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+        </div>
+    );
+}
